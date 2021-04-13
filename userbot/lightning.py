d69ee2a27e5653c1b947a976d89b4842b06bbdab
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-04-13 23:12:53 +0100

    Update lightning.py

--- userbot/lightning.py
+++ userbot/lightning.py
@@ -5,18 +5,18 @@
 #chala jaa bhosdike 
 import os
 from userbot import ALIVE_NAME 
-MASTER = str(ALIVE_NAME) if ALIVE_NAME else "LIGHTNING"
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "Ultimate"
 NAME = os.environ.get("BOT_NICK_NAME")
-BOT =  = str(NAME) if NAME else "BLACK LIGHTNING"
+BOT =  = str(NAME) if NAME else "Ultimate Bot"
 #op for keinshin
 
-Keinshin = "[KEINSHIN](https://t.me/keinshin)"
-OP = "[BLACK LIGHTNING](https://github.com/KeinShin/Black-Lightning)"
-OKAY = "[SUPPORT GROUP](https://t.me/LIGHTINGSUPPORT)"
+Keinshin = "[YOUNGCHRIS](https://t.me/youngchris112)"
+OP = "[ULTIMATE](https://github.com/chrisdroid1/Ultimate)"
+OKAY = "[SUPPORT GROUP](https://t.me/Ultim_ate)"
 #itna test h aur aage krte h
 #test successful raha ab aage 
-ALIVE = "BLACK LIGHTNING BOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22 for black lightning
-USERBOT = " HELLO MASTER MY NAME IS BLACK LIGHTNING BOT I AM A BEST USERBOT 💝"
+ALIVE = "ULTIMATE BOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22 for black lightning
+USERBOT = " HELLO MASTER MY NAME IS ULTIMATE BOT I AM A BEST USERBOT 💝"
 EMOJI = "⚡"
 #yrr isko apne bot me aply krne se pehle mere se pooch lena ok
 #aur aage add kruga abhi busy okay 🤔
