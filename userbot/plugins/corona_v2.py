cd8feae417894d8b14ceda71ca4f4a42dda98eae
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-04-14 03:41:53 +0100

    Update corona_v2.py

--- userbot/plugins/corona_v2.py
+++ userbot/plugins/corona_v2.py
@@ -29,7 +29,7 @@ async def _(event):
             return
         if response.text.startswith("Country"):
             await event.edit(
-                "😶**Country Not Found**😅\n\n[🔴🔴🔴🔴\n ⏩⏩ How to use ⏪⏪\n🔵🔵🔵🔵](https://t.me/blacklightningot)"
+                "😶**Country Not Found**😅\n\n[🔴🔴🔴🔴\n ⏩⏩ How to use ⏪⏪\n🔵🔵🔵🔵](https://t.me/Ult_imate)"
             )
         else:
             await event.delete()
