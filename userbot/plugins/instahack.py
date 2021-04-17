3a7411520af2008193ade7458633c3fa9a4e4cc6
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-04-17 21:33:38 +0100

    Create instahack.py

---
+++ userbot/plugins/instahack.py
@@ -0,0 +1,62 @@
+
+
+#Start 
+#Plugin by @chrisdroid1
+#kang =gey ,keep credits = cool programmer 😯
+#usage .instahack
+from telethon import events
+import asyncio
+from userbot.utils import lightning_cmd
+
+from userbot import CMD_HELP
+@borg.on(lightning_cmd(pattern=r"instahack"))
+async def hapy(event):
+      a="Starting INSTAGRAM hack😎⚡ [●]"
+      await event.edit(a)
+      b="Starting INSTAGRAM Hack😎⚡ [○]"
+      await event.edit(b)
+      await asyncio.sleep(2)
+      c="Connecting to INSTAGRAM servers 😎⚡ [°]"
+      await asyncio.sleep(1)
+      await event.edit(c) 
+      d="Conneced to INSTAGRAM [🤫] connecting to @Ultim_ate[•]"
+      await event.edit(d)
+      e="INFORMATION LOOTED FROM INSTAGRAM ✅ \n ■□□□□  \n [○]"
+      await event.edit(e)
+      f="ALL DETAILS LOCKED AND ACCESS HAS BEEN APPROVED✅ \n □■□□□  \n [●]"
+      await event.edit(f)
+      await asyncio.sleep(1)
+      await event.edit(e)
+      await event.edit(f)
+      g="Connected [👨‍💻] ⚡ \n Now finding **SECRET PASSCODE 💬 **[•]"
+      h="Finding**SECRET PASSCODE** [●]"
+      i="** SECRET PASSWORD** FOUNDED [●]"
+      await event.edit(g)
+      await asyncio.sleep(3)
+      await event.edit(h)
+      await asyncio.sleep(1)
+      await event.edit(i)
+      await asyncio.sleep(1)
+      j="LINK CLOAKING ■□□□□ [🕳]"
+      await event.edit(j)
+      await asyncio.sleep(1)
+      k="LINK CLOAKING □■□□□ [👨‍💻]"
+      await event.edit(k)
+      await asyncio.sleep(1)
+      op="URL MASKING □□■□□  [💬]"
+      await event.edit(op)
+      await asyncio.sleep(1)
+      yo="URL CLOAKING □□□■□  [😧]"
+      await event.edit(yo)
+      await asyncio.sleep(1)
+      re="CLOAKING URL□□□□■  [🤫]"
+      await event.edit(re)
+      await asyncio.sleep(1)
+      au="[DONE 🤓]\n SDCARD/STORAGE/INSTAGRAM DATA IS HACKED SUCCESSFUL 🔥😎⚡\n removing all Secret passcode"
+      await event.edit(au)
+      await asyncio.sleep(5)
+      wu="REMOVED ALL THE SECRET PASSCODE\n` NOW SEND ALL YOUR PROPERTIES TO MY MASTER `[TO GET ALL UR DETAILS BACK 😂😂😂 OR I WILL TAKE😯😯😯 ](https://telegra.ph/file/bedb506e7b88dbfe330c4.jpg)`AND POST TO EVERYBODY`"
+      await event.edit(wu)
+      
+      
+     
